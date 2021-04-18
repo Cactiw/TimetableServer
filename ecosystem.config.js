e2d52c5fcf8e9058561a5f9044cbf31b0a0cf3fb
Cactiw <Knyazev1514@gmail.com>
2021-04-18 16:25:49 +0300

    Added pm2 config

---
+++ ecosystem.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  apps : [{
+    name: 'TimetableServer',
+    script: 'app.py',
+    interpreter: 'python3',
+    log_file: 'timetable_server.log',
+    min_uptime: 5000,
+    kill_timeout: 15000,
+    max_restarts: 3,
+
+
+    // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
+    args: '-u',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '500M',
+    env: {
+      NODE_ENV: 'production'
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    }
+  }
+  ]
+};
