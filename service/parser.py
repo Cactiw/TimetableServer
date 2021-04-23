e79103bd0e39c5a60b1ef5500a96622dd246e9ad
Cactiw <Knyazev1514@gmail.com>
2021-04-23 14:07:56 +0300

    Added merged info to pair

--- service/parser.py
+++ service/parser.py
@@ -10,11 +10,11 @@ import pprint as pp
 
 
 def get_book_by_path(path):
-    return xlrd.open_workbook(path)
+    return xlrd.open_workbook(path, formatting_info=True)
 
 
 def get_book_by_file(file):
-    return xlrd.open_workbook(file_contents=file, on_demand=False)
+    return xlrd.open_workbook(file_contents=file, formatting_info=True, on_demand=False)
 
 
 def get_sheet(book, num):
@@ -23,7 +23,17 @@ def get_sheet(book, num):
 
 def get_day(sheet, num):
     # Getting raw day data
-    day_dirty = [[i for i in sheet.row_values(j)] for j in range(2 + num % 10 + num * 10, 13 + num % 10 + num * 10)]
+    merged_cells = sheet.merged_cells # x_start, x_end + 1, y_start, y_end + 1
+    day_dirty = []
+    for i in range(2 + num % 10 + num * 10, 13 + num % 10 + num * 10):
+        day_data = []
+        for j, value in enumerate(sheet.row_values(i)):
+            day_data.append((value, any(map(
+                lambda merge_data: merge_data[0] <= i < merge_data[1] and merge_data[2] <= j < merge_data[3],
+                merged_cells
+            ))))
+        day_dirty.append(day_data)
+    # day_dirty = [[i for i in sheet.row_values(j)] for j in range(2 + num % 10 + num * 10, 13 + num % 10 + num * 10)]
 
     day = [day_dirty[0]]
 
@@ -36,13 +46,13 @@ def get_day(sheet, num):
 
 
 def get_groups(sheet):
-    return get_day(sheet, 0)[0][1:]
+    return list(map(lambda text_merged: text_merged[0], get_day(sheet, 0)[0][1:]))
 
 
 def get_group_day(day, num):
     group = [day[0][0]]
     for i in range(1, len(day)):
-        group.append([day[i][0], day[i][1 + int(num) - int(day[0][1])]])
+        group.append([day[i][0], day[i][1 + int(num) - int(day[0][1][0])]])
     return group
 
 
@@ -63,7 +73,8 @@ def generate_pretty_group(l: list):
 
 
 def parse_pair(pair_data: list) -> dict:
-    pair_time, (pair_subject, pair_teacher) = pair_data
+    (pair_time, pair_time_merged), ((pair_subject, pair_subject_merged), (pair_teacher, pair_teacher_merger)) = \
+        pair_data
     pair_auditorium = None
 
     if not pair_subject and not pair_teacher:
@@ -75,7 +86,7 @@ def parse_pair(pair_data: list) -> dict:
         pair_time, pair_subject = corrected_time.groups()
 
     if not pair_teacher:
-        return make_dict(pair_time, pair_subject, pair_teacher, None)
+        return make_dict(pair_time, pair_subject, pair_teacher, None, pair_subject_merged)
 
     short_teacher = re.search("(\\w+ \\w\\.[\\w ]\\.?)\\s+(\\w+)", pair_teacher)
     if short_teacher is not None:
@@ -83,12 +94,15 @@ def parse_pair(pair_data: list) -> dict:
     else:
         pair_auditorium = pair_subject.split()[-1]
         pair_subject = " ".join(pair_subject.split()[:-1])
-    return make_dict(pair_time, pair_subject, pair_teacher, pair_auditorium)
+    return make_dict(pair_time, pair_subject, pair_teacher, pair_auditorium, pair_subject_merged)
 
 
-def make_dict(pair_time, pair_subject, pair_teacher, pair_auditorium) -> dict:
-    return {"time": pair_time, "subject": " ".join(pair_subject.split()).strip(),
-            "teacher": pair_teacher.strip(), "pair_auditorium": pair_auditorium}
+def make_dict(pair_time, pair_subject, pair_teacher, pair_auditorium, pair_subject_merged: bool = False) -> dict:
+    return {
+        "time": pair_time, "subject": " ".join(pair_subject.split()).strip(),
+        "teacher": pair_teacher.strip(), "pair_auditorium": pair_auditorium,
+        "merged": pair_subject_merged
+    }
 
 
 def parse_xls(file=None, path=None):
